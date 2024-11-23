c00fcad42ab41107f9ef3942e1612911a6d03b08
Bui Cong Bac <buicongbac182004@gmail.com>
2024-11-23 09:44:31 +0700

    Send Email

---
+++ btljavafinal/spring-boot-example/src/main/java/spring/example/controller/EmailController.java
@@ -0,0 +1,20 @@
+package spring.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import spring.example.service.EmailService;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @GetMapping("/send-email")
+    public String sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String text) {
+        emailService.sendSimpleEmail(to, subject, text);
+        return "Email sent successfully!";
+    }
+}
